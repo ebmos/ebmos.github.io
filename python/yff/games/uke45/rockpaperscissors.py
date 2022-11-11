57a07b7cee2dc4845edfe3b783c2e6a9bb898412
ebmos <ebmos2021@gmail.com>
2022-11-11 09:44:06 +0100

    oh yeah

--- python/yff/games/uke45/rockpaperscissors.py
+++ python/yff/games/uke45/rockpaperscissors.py
@@ -18,6 +18,8 @@ if playing == "no":
 if playing not in yesno:
     print("Feil input.")
 
+print("Whoever gets 3 points first, wins.")
+
 while True:
 
     play = input(Fore.WHITE + "Rock paper or scissors? (enter quit to quit) ").lower()
@@ -32,6 +34,7 @@ while True:
 
     randomnumber = random.randint(0,2)
     compChose = options[randomnumber]
+
 
     if play == "rock" and compChose == "scissors":
         print(Fore.GREEN + "You won!")
@@ -48,13 +51,18 @@ while True:
         print(Fore.WHITE + "The computer chose", compChose)
         userWins += 1
 
+    elif play == compChose:
+        print(Fore.BLUE + "You chose the same.")
+        continue
+
     else:
         print(Fore.RED + "You lost.")
         print(Fore.WHITE + "The computer chose", compChose)
+        compWins += 1
 
     if compWins == 3:
         print(Fore.RED + "You lost the bo3. Your score was", userWins)
-        break
-    else: 
+        quit()
+    elif userWins == 3: 
         print(Fore.YELLOW + "You won the bo3! Your score was", userWins)
-        break
+        quit()
